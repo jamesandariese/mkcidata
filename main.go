59d3cc84b9ee1307d37f795899b5368da654a93e
James Andariese <james@strudelline.net>
2023-02-10 21:45:24 -0600

    fix flag issue so file may be named

--- main.go
+++ main.go
@@ -14,7 +14,11 @@ var (
 
 func main() {
 	flag.Parse()
-	err := mkiso.CreateIso("poop.iso", flag.Args())
+	args := flag.Args()
+	if len(args) < 2 {
+		log.Fatalf("usage: mkcidata thing.iso filea [fileb] [filec]")
+	}
+	err := mkiso.CreateIso(args[0], args[1:])
 	if err != nil {
 		log.Fatalf("Fatal error making ISO: %v", err)
 	}
